a1c99f274d7885d47d7228fcb0a8caca254ff4e5
Andy <andysctu@users.noreply.github.com>
2015-04-23 14:24:03 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 function getRunes(){
     aClient = new HttpClient();
     aClient.get('https://na.api.pvp.net/api/lol/na/v1.4/summoner/48357/runes?api_key=a6e94ee5-fa0b-41e1-9cac-271e7212b875', function(response) {
@@ -6,14 +5,9 @@ function getRunes(){
         alert(response)
     });
     
+    return response;
+    
 }
-function getID(){
-    var name = httpGet('https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/ihatedisgame?api_key=a6e94ee5-fa0b-41e1-9cac-271e7212b875');
-    alert(name.id);
-    return name;
-}
-
-
 
 function httpGet(theUrl) {
     var xmlHttp = null;
