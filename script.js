ca54f4f85ae6e1b27a058f73972d09688a9f5d3f
Andy <andysctu@users.noreply.github.com>
2015-04-23 10:27:48 -0400

    Update script.js

--- script.js
+++ script.js
@@ -11,4 +11,5 @@ function httpGet(theUrl)
 function getID(){
     var name = httpGet('https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/ihatedisgame?api_key=a6e94ee5-fa0b-41e1-9cac-271e7212b875');
     alert(name);
+    return name;
 }
